78ac7ec8934afa26622ec776cb914ad2119d5eb3
DannyDahNerd <68573397+DannyDahNerd@users.noreply.github.com>
2020-08-13 11:38:09 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("I play saxophone");
+		System.out.println("I want to major in CS");
+		System.out.println("I play alot of minecraft");
+		System.out.println("I would like to go to UC Berekly");
+		System.out.println("I have a dog named DJ");
+	}
+
+}
